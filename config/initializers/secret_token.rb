57dfadc11866670d731b62c5a91ca6e65cd2e306
Ravi Desai <ravi@ravidesai.com>
2014-08-31 14:24:16 -0400

    Initial commit
    - not tracking database.yml

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CanadaCensus::Application.config.secret_key_base = '40e1560e5b692d3d072875f466b4e01dd3c07d1163079dc646c671fbadba0372669796a5ba9a26c66e6659170de730117b74d6a6aa8dfa899d46a651a87f1e14'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CanadaCensus::Application.config.session_store :cookie_store, key: '_canada_census_session'
